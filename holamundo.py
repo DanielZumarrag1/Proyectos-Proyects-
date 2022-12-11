45b65d47e47db1e12cd82e73ab66190293764c5f
Daniel Zumárraga <120277197+VanLeidan5858@users.noreply.github.com>
2022-12-11 10:44:54 -0500

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,6 @@
+# Este es un comentario.
+# Este codigo imprime un mensaje por la pantalla.
+# Hello world es un string (string es una cadena de caracteres) (hasta un espacio en blanco es un caracter)
+# Print es una función.
+
+print ("Hola Mundo")
